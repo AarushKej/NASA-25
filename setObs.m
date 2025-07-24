b3730b44317fe798b8624dec816efecc749f09f3
Kristiyan Donchev <76012241+ArKayD07@users.noreply.github.com>
2025-07-24 10:28:23 -0400

    forgot to add other files

--- setObs.m
+++ setObs.m
@@ -1,9 +1,27 @@
-function plots=setObs(plots,obsSet)
-assert(length(plots.poly.obsSets)>=obsSet,'Obstacle set does not exist.')
-assert(~isempty([plots.poly.obsSets{obsSet}]),'Obstacle set does not exist.')
-assert(~isempty(vertcat(plots.poly.obsSets{1}.Vertices)),'Obstacle set does not exist.')
-allobs=union(plots.poly.obsSets{obsSet});
-plots.poly.obsplt.Shape=allobs;
-plots.poly.track=union(plots.poly.walls,allobs);
-drawnow
+function plots = setObs(plots, obsSet)
+    % Validate inputs
+    assert(length(plots.poly.obsSets) >= obsSet, 'Obstacle set does not exist.');
+    assert(~isempty(plots.poly.obsSets{obsSet}), 'Obstacle set does not exist.');
+    assert(~isempty(vertcat(plots.poly.obsSets{obsSet}.Vertices)), 'Obstacle set does not exist.');
+
+    % Union all obstacles in this set
+    allobs = union(plots.poly.obsSets{obsSet});
+
+    % If a plot handle already exists, update its shape
+    if isfield(plots.poly, 'obsplt') && isgraphics(plots.poly.obsplt)
+        plots.poly.obsplt.Shape = allobs;
+    else
+        % Create a new plot handle if none exists
+        plots.poly.obsplt = plot(plots.ax, allobs, ...
+            'FaceColor', [1 0 0], 'FaceAlpha', 0.2, 'EdgeColor', 'k');
+    end
+
+    % Update track (walls + obstacles)
+    if isfield(plots.poly, 'walls')
+        plots.poly.track = union(plots.poly.walls, allobs);
+    else
+        plots.poly.track = allobs;
+    end
+
+    drawnow;
 end
